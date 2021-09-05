12c8d4b75aff7dc9d5e9674597117c80ef41d5a4
alperin35 <65857758+alperin35@users.noreply.github.com>
2021-09-05 17:38:34 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -2,4 +2,4 @@ let ad = prompt(`Adınız: `)
 let soyad = prompt(`Soyadınız: `)
 if ((ad == "Kübra" || ad == "kübra") && (soyad == "inan" || soyad == "İnan")) {
     window.alert(`Hoşgeldin hatunum ❤`)
-} else window.alert(`İyi çalışmalar ${ad} ${soyad} :) ❤`)
+} else window.alert(`Hoşgeldin ${ad} ${soyad}!`)
