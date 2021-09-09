4d445bbcee27e7a7bdf51661404853e84d789851
alperin35 <65857758+alperin35@users.noreply.github.com>
2021-09-10 01:16:38 +0300

    Delete js.js

--- js.js
+++
@@ -1,5 +0,0 @@
-let ad = prompt(`Adınız: `)
-let soyad = prompt(`Soyadınız: `)
-if ((ad == "Kübra" || ad == "kübra") && (soyad == "inan" || soyad == "İnan")) {
-    window.alert(`Hoşgeldin hatunum ❤`)
-} else window.alert(`Hoşgeldin ${ad} ${soyad}!`)
